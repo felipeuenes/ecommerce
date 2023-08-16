d442e010881b187d09c81a0297ac41e95cbdd9f8
Felipe Uenes <uenesdesigner@gmail.com>
2023-08-16 12:01:00 -0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import { Top } from './components/cabecalho'
 
 export function App() {
 
 
   return (
     <>
-   
+      <Top/>
     </>
   )
 }
---
+++ src/components/cabecalho/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+
+export function Top(){
+
+    return(
+        <div className="top">
+
+        </div>
+    )
+}
