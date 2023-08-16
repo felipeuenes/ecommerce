9d834264c02744de70c3f58af70c4f42a66f9a7d
Felipe Uenes <uenesdesigner@gmail.com>
2023-08-16 13:14:14 -0300

    test

--- src/components/produtos/index.jsx
+++ src/components/produtos/index.jsx
@@ -7,6 +7,14 @@ export function Produtos() {
     return(
         <div className='produtos'>
 
+            <div className='linha-produtos'>
+                <section className='produto'>
+                    <img src="https://images4.kabum.com.br/produtos/fotos/235984/placa-de-video-asrock-amd-radeon-rx-6600-cld-8g-8gb-90-ga2rzz-00uanf_1634738812_m.jpg" alt="" />
+                </section>
+                <section className='produto'></section>
+                <section className='produto'></section>
+                <section className='produto'></section>
+            </div>
         </div>
     )
 }
