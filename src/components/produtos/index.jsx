2ec69dcd09cdf17c6aab41f1a2ad9087ab506a6e
Felipe Uenes <uenesdesigner@gmail.com>
2023-08-22 20:55:27 -0300

    add images

--- src/components/produtos/index.jsx
+++ src/components/produtos/index.jsx
@@ -11,9 +11,15 @@ export function Produtos() {
                 <section className='produto'>
                     <img src="https://images4.kabum.com.br/produtos/fotos/235984/placa-de-video-asrock-amd-radeon-rx-6600-cld-8g-8gb-90-ga2rzz-00uanf_1634738812_m.jpg" alt="" />
                 </section>
-                <section className='produto'></section>
-                <section className='produto'></section>
-                <section className='produto'></section>
+                <section className='produto'>
+                    <img src="https://images9.kabum.com.br/produtos/fotos/476739/console-sony-playstation-5-marvel-s-spider-man-2-limited-edition-cfi-1214az2x_1690549759_m.jpg" alt="" />
+                </section>
+                <section className='produto'>
+                    <img src="https://images8.kabum.com.br/produtos/fotos/sync_mirakl/399908/Notebook-Vaio-Fe15-AMD-Ryzen-7-5700U-8GB-RAM-SSD-512GB-15-6-Full-HD-Windows-11-Home-Prata-Tit-nio_1686578367_m.jpg" alt="notebook" />
+                </section>
+                <section className='produto'>
+                    <img src="https://images6.kabum.com.br/produtos/fotos/134176/cadeira-gamer-tempest-husky-gaming-black-black-700_1619705487_m.jpg" alt="cadeira-gamer" />
+                </section>
             </div>
         </div>
     )
