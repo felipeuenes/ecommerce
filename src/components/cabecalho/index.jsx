8b058c22f809c63ce1d598bc83af38feb9e659e3
Felipe Uenes <uenesdesigner@gmail.com>
2023-08-22 21:02:07 -0300

    teste

--- src/components/cabecalho/index.jsx
+++ src/components/cabecalho/index.jsx
@@ -5,7 +5,13 @@ export function Top(){
 
     return(
         <div className="top">
-
+            <section>
+                <input type="text" placeholder="Pesquisar"/>
+                <button>Buscar</button>
+            </section>
+            <section>
+                <p>Faça <a href="#">LOGIN</a> ou <br/>crie o seu <a href="#">CADASTRO</a></p>
+            </section>
         </div>
     )
 }
